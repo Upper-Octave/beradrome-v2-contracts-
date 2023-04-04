c7910a59c8b720620e9b417408caa5d360a695d0
issawayzani <38286051+issawayzani@users.noreply.github.com>
2023-04-05 00:28:07 +0300

    Update and rename Velo.sol to BERO.sol

--- contracts/BERO.sol
+++ contracts/BERO.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.13;
 
-import "contracts/interfaces/IVelo.sol";
+import "contracts/interfaces/IBERO.sol";
 
-contract Velo is IVelo {
+contract BERO is IBERO {
 
-    string public constant name = "Velodrome";
-    string public constant symbol = "VELO";
+    string public constant name = "Beradrome";
+    string public constant symbol = "BERO";
     uint8 public constant decimals = 18;
     uint public totalSupply = 0;
 
